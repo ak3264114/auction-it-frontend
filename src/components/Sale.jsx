4bf39cf938a15db7597ba858ae85ecd82bd917f4
amit <ak3264114@gmail.com>
2023-10-28 13:16:57 +0530

    skelton added

--- src/components/Sale.jsx
+++ src/components/Sale.jsx
@@ -33,23 +33,78 @@ const Sale = () => {
 	}, []);
 
 	return !auctionItem ? (
-		<div className="mt-3 p-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-			<div class=" shadow rounded-md p-4 max-w-sm w-full mx-auto">
-				<div class="rounded-lg bg-slate-200 h-36 w-full"></div>
-				<div class="animate-pulse flex space-x-4">
-					<div class="flex-1 space-y-6 py-1">
-						<div class="h-2 bg-slate-200 rounded"></div>
-						<div class="space-y-3">
-							<div class="grid grid-cols-3 gap-4">
-								<div class="h-2 bg-slate-200 rounded col-span-2"></div>
-								<div class="h-2 bg-slate-200 rounded col-span-1"></div>
+		<>
+			<div className="flex justify-center mt-5">
+				<h1 className="text-slate-900 text-3xl font-mono font-bold">Items</h1>
+			</div>
+
+			<div className="mt-3 p-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+				<div class=" shadow rounded-md p-4 max-w-sm w-full mx-auto">
+					<div class="rounded-lg bg-slate-200 h-36 w-full"></div>
+					<div class="animate-pulse flex space-x-4">
+						<div class="flex-1 space-y-6 py-1">
+							<div class="space-y-3 mt-3">
+								<div class="h-5 bg-slate-200 rounded "></div>
+								<div class="h-3 bg-slate-200 rounded "></div>
+								<div class="h-3 bg-slate-200 rounded "></div>
 							</div>
-							<div class="h-2 bg-slate-200 rounded"></div>
+							<div className="flex justify-between">
+								<div class="h-10 w-20 bg-slate-200 rounded"></div>
+								<div class="h-10 w-20 bg-slate-200 rounded float-right"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class=" shadow rounded-md p-4 max-w-sm w-full mx-auto">
+					<div class="rounded-lg bg-slate-200 h-36 w-full"></div>
+					<div class="animate-pulse flex space-x-4">
+						<div class="flex-1 space-y-6 py-1">
+							<div class="space-y-3 mt-3">
+								<div class="h-5 bg-slate-200 rounded "></div>
+								<div class="h-3 bg-slate-200 rounded "></div>
+								<div class="h-3 bg-slate-200 rounded "></div>
+							</div>
+							<div className="flex justify-between">
+								<div class="h-10 w-20 bg-slate-200 rounded"></div>
+								<div class="h-10 w-20 bg-slate-200 rounded float-right"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class=" shadow rounded-md p-4 max-w-sm w-full mx-auto">
+					<div class="rounded-lg bg-slate-200 h-36 w-full"></div>
+					<div class="animate-pulse flex space-x-4">
+						<div class="flex-1 space-y-6 py-1">
+							<div class="space-y-3 mt-3">
+								<div class="h-5 bg-slate-200 rounded "></div>
+								<div class="h-3 bg-slate-200 rounded "></div>
+								<div class="h-3 bg-slate-200 rounded "></div>
+							</div>
+							<div className="flex justify-between">
+								<div class="h-10 w-20 bg-slate-200 rounded"></div>
+								<div class="h-10 w-20 bg-slate-200 rounded float-right"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class=" shadow rounded-md p-4 max-w-sm w-full mx-auto">
+					<div class="rounded-lg bg-slate-200 h-36 w-full"></div>
+					<div class="animate-pulse flex space-x-4">
+						<div class="flex-1 space-y-6 py-1">
+							<div class="space-y-3 mt-3">
+								<div class="h-5 bg-slate-200 rounded "></div>
+								<div class="h-3 bg-slate-200 rounded "></div>
+								<div class="h-3 bg-slate-200 rounded "></div>
+							</div>
+							<div className="flex justify-between">
+								<div class="h-10 w-20 bg-slate-200 rounded"></div>
+								<div class="h-10 w-20 bg-slate-200 rounded float-right"></div>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	) : (
 		<>
 			<div className="flex justify-center mt-5">
